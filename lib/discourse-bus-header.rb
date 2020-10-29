59232d2f0d96f84d6a226d17b09edf06cfe4295a
Maximilien Pottiez <pottiez.maximilien@gmail.com>
2020-10-29 16:46:47 +0000

    additional check

--- lib/discourse-bus-header.rb
+++ lib/discourse-bus-header.rb
@@ -1,11 +1,16 @@
 begin
     require 'message_bus'
-    puts 'Add message bus header.'
-    MessageBus.extra_response_headers_lookup do |env|
-        [
-            ["Access-Control-Allow-Origin", SiteSetting.discourse_server_environment],
-        ]
-      end
+    after_initialize do
+        puts 'Message Bus Plugin'
+        if SiteSetting.discourse_server_environment
+            puts 'Add message bus header'
+            MessageBus.extra_response_headers_lookup do |env|
+                [
+                    ["Access-Control-Allow-Origin", SiteSetting.discourse_server_environment],
+                ]
+            end
+        end
+    end
 rescue LoadError
     puts 'Message Bus is not loaded, plugin Message bus header failed'
 end
