73f8a61faf6455d1ca45789f6e32fe8a90712b67
Maximilien Pottiez <pottiez.maximilien@gmail.com>
2020-10-29 16:56:22 +0000

    additional config

--- lib/discourse-bus-header.rb
+++ lib/discourse-bus-header.rb
@@ -2,7 +2,7 @@ begin
     require 'message_bus'
     after_initialize do
         puts 'Message Bus Plugin'
-        if SiteSetting.discourse_server_environment
+        if SiteSetting.discourse_bus_header_enabled
             puts 'Add message bus header'
             MessageBus.extra_response_headers_lookup do |env|
                 [
